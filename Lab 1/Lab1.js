da63ced0cb2fc2a028cb0729a6e1592f39d7cb95
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2024-09-27 13:29:43 +0000

    Question 2d
    Added an arrows function that multplies the given number by two if it's under 70.

--- Lab 1/Lab1.js
+++ Lab 1/Lab1.js
@@ -12,3 +12,16 @@ let sum = (a, b) =>
 }
 
 console.log(sum(6, 4));
+
+
+const ages = [25, 31, 42, 77];
+
+let under70 = (age) =>
+{
+    if(age < 70)
+        return age * 2;
+    else
+        return age;
+}
+
+console.log(ages.map((age) => under70(age)));
