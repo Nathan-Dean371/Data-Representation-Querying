90ec9a25c3ea4c440066612d5deca8f927045c6b
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2024-09-27 13:39:09 +0000

    Added listAllTasks function

--- Lab 1/Question3.js
+++ Lab 1/Question3.js
@@ -1,3 +1,17 @@
-const qArray = [];
+const qArray = [ "Task 1", "Task 2", "Task 3"];
 
 
+let addTask = (task) =>
+{
+    qArray.push(task);
+    console.log("Pushed task: " + task + " onto the array.");
+    return qArray.length;
+}
+
+let listAllTasks = () => qArray.forEach((task) =>
+{
+ console.log(task);
+})
+
+addTask("Task 4");
+listAllTasks();
