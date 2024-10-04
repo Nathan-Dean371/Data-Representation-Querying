763666e9b20811997731ef9e2e57dd9eada9c2c9
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2024-10-04 13:56:02 +0000

    Updated App.js
    Changed the elements in the Routes to display previously created components when the Url changes.

--- Lab 2/my-app/src/App.js
+++ Lab 2/my-app/src/App.js
@@ -14,8 +14,10 @@ function App() {
         <NavigationBar/>
         <Routes>
           <Route path="/home" element={<Content />} />
-          <Route path="/read" element={<h1>Read Component</h1>} />
-          <Route path="/create" element={<h1>Create Component</h1>} />
+          <Route path="/read" element={<Footer/>} />
+          
+          <Route path="/create" element={<Header/>} />
+
         </Routes>
       </Router>      
     </div>
