58004ebd763544ead835baa66800c442f50543c5
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2024-10-11 15:06:49 +0100

    Task 2
    Added movie data to Read component.
    Created Movie component and passed data from Read component and displayed the movie data in the console.

---
+++ Lab 2/my-app/src/components/Movies.js
@@ -0,0 +1,11 @@
+const Movies = (data) =>
+{
+    return(
+        <div>
+            <h1>Hello from movie component</h1>
+            {console.log(data.myMovies)}
+        </div>
+    )
+}
+
+export default Movies
