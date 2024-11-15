fed724137ab2ffdbf6eaf541413ef96f41c9dced
Nathan Dean <G00423489@atu.ie>
2024-11-15 10:41:59 +0000

    Added GET method to retrieve all movies

--- Lab 7/backend/server.js
+++ Lab 7/backend/server.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
 mongoose.connect('mongodb+srv://admin:admin@lab7-cluster.khz7c.mongodb.net/?retryWrites=true&w=majority&appName=Lab7-Cluster');
 
 
@@ -28,10 +32,13 @@ app.post("/api/movies", async (req, res) => {
 
 });
 
+app.get("/api/movies", async (req, res) => {
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+  const movies = await Movie.find();
+  res.json(movies);
+
+});
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -48,35 +55,5 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.get("/api/movies", (req, res) => {
-  const movies = [
-    {
-      Title: "Avengers: Infinity War (server)",
-      Year: "2018",
-      imdbID: "tt4154756",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Captain America: Civil War (server)",
-      Year: "2016",
-      imdbID: "tt3498820",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg",
-    },
-    {
-      Title: "World War Z (server)",
-      Year: "2013",
-      imdbID: "tt0816711",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg",
-    },
-  ];
-
-  res.json({ movies });
-});
 
 
